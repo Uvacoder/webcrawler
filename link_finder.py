ac50613809f833b2de7954e85b2cbc5c3d2c8267
Mitul Shah <mitulxshah@gmail.com>
2017-12-27 23:07:01 -0500

    link finder complete, with loops for anchor tags

--- link_finder.py
+++ link_finder.py
@@ -6,11 +6,24 @@ from urllib import parse
 
 class LinkFinder(HTMLParser):
 
-    def __init__(self):
+    def __init__(self, base_url, page_url):
         super().__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
 
-    def handle_startendtag(self, tag, attrs):
-        print(tag)
+    def handle_starttag(self, tag, attrs):
+        # If tag is anchor tag -> check if attributes are href
+        # For a tag, check the href and value
+        # With the value, join it to the base url and add it to the set
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+    def page_links(self)
+        return self.links
 
     # If there's any errors, catch it and deliver the message
     def error(self, message):
